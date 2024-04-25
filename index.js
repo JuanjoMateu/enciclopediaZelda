9976b27fe64ee34eaade7a942269bd908b86db6e
JuanjoMateu <juanjo_mateu@hotmail.es>
2024-04-25 11:01:23 +0200

    Vinculación a BBDD

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const dbConnection = require('./config/config')
 const app = express()
+require('dotenv').config()
 
-const PORT = 3000
+
+const PORT = process.env.PORT || 4000
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => { res.send('Enciclopedia Zelda') })
+
+dbConnection()
 
 app.listen(PORT, () => {
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
